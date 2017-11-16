f55b666c1f3c4e31872b1f7378963ee89dcd9a37
Michael DAntuono <mikeishere3@gmail.com>
2017-11-16 20:46:39 +0000

    second commit, functions dont fully work yet

--- cars.js
+++ cars.js
@@ -1,50 +1,43 @@
-var cars = {
-    name: 'Enterprise Rental Cars',
-    type1: 'Economy',
-    // economyTotal: 100,
-    // economyRented: function() {
-    //     return this.economyTotal - this.economyAvail();
-    // },
-    economyAvail: 100,
-    economyRented: 0,
-    economyLeft: function() {
-        return this.economyAvail - this.economyRented;
-    },
-    
-    type2: 'Midsize',
-    // midsizeTotal: 100,
-    // midsizeRented: function() {
-    //     return this.midsizeTotal - this.midsizeAvail();
-    // },
-    midsizeAvail: 100,
-    midsizeRented: 0,
-    midsizeLeft: function() {
-        return this.midsizeAvail - this.midsizeRented;
-    },
+var carRental = {
+    name: 'Rent-A-Car',
+    carType: [
+        {
+            name: "Economy",
+            price: 50,
+            amount: 10,
+        },
+        {
+            name: "Midsize",
+            price: 80,
+            amount: 10,
+        },
+        {
+            name: "Luxury",
+            price: 120,
+            amount: 10,
+        }
+    ],
 };
 
-function cardata() {
-    document.getElementById('business').innerHTML = cars.name;
-    document.getElementById('type1').innerHTML = cars.type1;
-    document.getElementById('type2').innerHTML = cars.type2;
-    document.getElementById('economy').innerHTML = cars.economyLeft();
-    document.getElementById('midsize').innerHTML = cars.midsizeLeft();
-  }
-// function rentEcon() {
-//     document.getElementById('economy').innerHTML = cars.economyAvail--;
-// }
-// function rentMidsize() {
-//     document.getElementById('midsize').innerHTML = cars.midsizeAvail--;
-// }
-
-function rentEcon() {
-    cars.economyRented++;
-    return cardata();
-}
-function rentMidsize() {
-    cars.midsizeRented++;
-    return cardata();
-}
-function destroy() {
-    document.getElementById('everything').innerHTML = "Goodbye."
-}
+document.ready(function() {
+    function display() {
+        var selection = carRental.carType[val];
+        document.getElementById('details').innerHTML = selection
+    }
+    function rentEconomy() {
+        carRental.carType[0].amount--;
+        console.log(carRental.carType[0].amount);
+    }
+    function rentMidsize() {
+        carRental.carType[1].amount--;
+        console.log(carRental.carType[1].amount);
+    }
+    function RentLuxury() {
+        carRental.carType[2].amount--;
+        console.log(carRental.carType[2].amount);
+    }
+    
+    function reserved() {
+        document.getElementById('pagecontent').innerHTML = "<h2>Reservation booked! Enjoy your ride.</h2>";
+    }
+})
